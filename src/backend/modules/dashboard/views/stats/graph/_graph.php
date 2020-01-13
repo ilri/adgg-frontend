315a4053af721a64e5b25aa4c77008a26b2bdd8c
Eric <eric@btimillman.com>
2020-01-13 21:50:17 +0300

    dashboard summary

---
+++ src/backend/modules/dashboard/views/stats/graph/_graph.php
@@ -0,0 +1,28 @@
+<?php
+
+use common\helpers\Lang;
+use common\widgets\highchart\HighChart;
+
+/* @var $graphFilterOptions array */
+/* @var $this \yii\web\View */
+?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="accordion" id="animal-stats-accordion" role="tablist">
+            <div class="card">
+                <h4 class="card-header" role="tab" id="headingOne" style="padding: 10px;">
+                    <a role="button" data-toggle="collapse" data-parent="#savings-stats-accordion"
+                       href="#savings-stats-collapse">
+                        <i class="glyphicon glyphicon-chevron-right"></i> <?= Lang::t('Animal Graphical Stats') ?>
+                    </a>
+                </h4>
+
+                <div id="savings-stats-collapse" class="panel-collapse collapse show" role="tabpanel">
+                    <div class="card-body">
+                        <?= $this->render('_widget', ['graphType' => HighChart::GRAPH_PIE, 'graphFilterOptions' => $graphFilterOptions]) ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/backend/modules/dashboard/views/stats/graph/_template.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $this \yii\web\View */
+/* @var $filterOptions array */
+?>
+{filter_form}
+<hr/>
+<div class="row">
+    <div class="col-md-2 hidden" id="animal-summary-stats">
+        <?= $this->render('_stats', ['filters' => $filterOptions]) ?>
+    </div>
+    <div class="col-md-10">
+        <div class="well well-sm well-light">
+            {chart}
+        </div>
+    </div>
+</div>
