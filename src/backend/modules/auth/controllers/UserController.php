9faf0d3327f7b03a6567d977094ebbc855ebcc2c
Eric <eric@btimillman.com>
2020-02-03 17:18:21 +0300

    show labels in place of selected fields

--- src/backend/modules/auth/controllers/UserController.php
+++ src/backend/modules/auth/controllers/UserController.php
@@ -260,7 +260,7 @@ class UserController extends Controller
 
     public function actionUploadPreview($level_id, $org_id = null)
     {
-        $form = new UploadUsers(['level_id' => $level_id, 'org_id' => $org_id]);
+        $form = new UploadUsers(Users::class, ['level_id' => $level_id, 'org_id' => $org_id]);
         return $form->previewAction();
     }
 }
