aa6c6bd55d47cc4375d2354c63dd1ee3efe3ae67
Jacktone Oriko <jacktone@competamillman.co.ke>
2020-02-07 13:47:40 +0300

    minor fix

--- src/backend/views/layouts/_menu.php
+++ src/backend/views/layouts/_menu.php
@@ -92,7 +92,7 @@ $countries = Organization::find()->orderBy(['code' => SORT_ASC])->all();
                             <?php foreach ($countries as $country): ?>
                                 <?php if (Session::getOrgId() == $country->id || Session::isPrivilegedAdmin()): ?>
                                     <li class="kt-menu__item">
-                                        <a href="<?= Url::to(['/core/farm/index', 'org_id' => $country->id]) ?>"
+                                        <a href="<?= Url::to(['/core/herd/index', 'org_id' => $country->id]) ?>"
                                            class="kt-menu__link ">
                                             <i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i>
                                             <span class="kt-menu__link-text"><?= Lang::t('{country}', ['country' => $country->name]) ?></span>
