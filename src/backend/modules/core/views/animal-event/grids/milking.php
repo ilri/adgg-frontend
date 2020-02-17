47c8bf1db7041ec07fa0331737ab62a79d2c5c59
Jacktone Oriko <jacktone@competamillman.co.ke>
2020-02-17 16:22:23 +0300

    fixed uploading events error

--- src/backend/modules/core/views/animal-event/grids/milking.php
+++ src/backend/modules/core/views/animal-event/grids/milking.php
@@ -14,7 +14,7 @@ use yii\helpers\Url;
     'searchModel' => $model,
     'createButton' => ['visible' => Yii::$app->user->canCreate() && false, 'modal' => false],
     'toolbarButtons' => [
-        Yii::$app->user->canCreate() ? '<a class="btn btn-brand btn-bold btn-upper btn-font-sm btn-space" href="' . Url::to(array_merge(['upload'], Yii::$app->request->queryParams)) . '" data-pjax="0"><i class="fa fa-file-excel-o"></i> ' . Lang::t('Upload Excel/CSV') . '</a> ' : '',
+        Yii::$app->user->canCreate() ? '<a class="btn btn-brand btn-bold btn-upper btn-font-sm btn-space" href="' . Url::to([$upload_url]) . '" data-pjax="0"><i class="fa fa-file-excel-o"></i> ' . Lang::t('Upload Excel/CSV') . '</a> ' : '',
     ],
     'columns' => [
         [
