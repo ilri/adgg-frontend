833340d5f05e2cc8617c0a1bc588b6f8b538b0ae
Fred <mconyango@gmail.com>
2020-02-18 15:05:30 +0300

    Migrating Mistro KE KLBA Data

---
+++ src/console/controllers/DataMigrationController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: Fred <mconyango@gmail.com>
+ * Date: 2020-02-18
+ * Time: 12:10 PM
+ */
+
+namespace console\controllers;
+
+
+use console\dataMigration\ke\models\Bulls;
+use yii\console\Controller;
+
+class DataMigrationController extends Controller
+{
+    public function actionRun()
+    {
+        $time_start = microtime(true);
+        $this->doMigration();
+        $time_end = microtime(true);
+        $executionTime = round($time_end - $time_start, 2);
+        $this->stdout("KE KLBA DATA MIGRATION EXECUTED IN {$executionTime} SECONDS\n");
+    }
+
+    protected function doMigration()
+    {
+        $this->doKeMigration();
+    }
+
+    protected function doKeMigration()
+    {
+        $this->migrateKeBulls();
+    }
+
+    protected function migrateKeBulls()
+    {
+        $query = Bulls::find()->andWhere([]);
+        /* @var $models Bulls[] */
+        $n = 1;
+        foreach ($query->batch() as $i => $models) {
+            foreach ($models as $model) {
+                $this->stdout("Bulls: Processing Record {$n}\n");
+                $n++;
+            }
+        }
+    }
+}
