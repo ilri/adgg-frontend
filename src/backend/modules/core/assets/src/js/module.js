691cc4481e7b17fddf3d628d49a3e05e66f5724d
Fred <mconyango@gmail.com>
2020-03-25 17:38:35 +0300

    ODK JSON Processing: Improvement on odk data view.

--- src/backend/modules/core/assets/src/js/module.js
+++ src/backend/modules/core/assets/src/js/module.js
@@ -42,3 +42,59 @@ MyApp.modules.core = {};
         obj.toggleListTypeField();
     }
 }(jQuery));
+
+//show odk json data
+(function ($) {
+    'use strict';
+
+    function ODK_DATA_VIEW(options) {
+        let defaultOptions = {
+            odkJsonDataWrapperSelector: '.show-pretty-json',
+        };
+        this.options = $.extend({}, defaultOptions, options || {});
+    }
+
+    let showPretty = function (el) {
+        let data = $(el).data('json');
+        if (MyApp.utils.empty(data)) {
+            return;
+        }
+
+        function syntaxHighlight(json) {
+            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+                var cls = 'number';
+                if (/^"/.test(match)) {
+                    if (/:$/.test(match)) {
+                        cls = 'key';
+                    } else {
+                        cls = 'string';
+                    }
+                } else if (/true|false/.test(match)) {
+                    cls = 'boolean';
+                } else if (/null/.test(match)) {
+                    cls = 'null';
+                }
+                return '<span class="' + cls + '">' + match + '</span>';
+            });
+        }
+
+        if (typeof data === "string") {
+            data = JSON.parse(data);
+        }
+        let str = JSON.stringify(data, undefined, 4);
+        $(el).html(syntaxHighlight(str));
+    }
+
+    ODK_DATA_VIEW.prototype.showPretty = function () {
+        let $this = this;
+        $($this.options.odkJsonDataWrapperSelector).each(function (i, obj) {
+            showPretty.call($this, obj);
+        });
+    }
+    MyApp.modules.core.showPrettyOdkJson = function (options) {
+        let obj = new ODK_DATA_VIEW(options);
+        obj.showPretty();
+    }
+
+}(jQuery));
