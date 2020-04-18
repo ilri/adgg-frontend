9c11111aafbcffb99d362c3e991cbdf2cad0dfad
Eric <eric@btimillman.com>
2020-04-18 15:57:13 +0300

    add table data to new dashboard design

--- src/backend/modules/dashboard/views/data-viz/partials/table.php
+++ src/backend/modules/dashboard/views/data-viz/partials/table.php
@@ -1,8 +1,20 @@
 <?php
+
+use backend\modules\core\models\Animal;
+use backend\modules\core\models\Choices;
+use backend\modules\core\models\ChoiceTypes;
+use backend\modules\core\models\Country;
+use backend\modules\core\models\Farm;
+use backend\modules\core\models\Organization;
 use common\helpers\Lang;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $filterOptions array */
+
+$countries = Country::getListData('id', 'name', false);
+$projects = Choices::getList(ChoiceTypes::CHOICE_TYPE_PROJECT, false);
+$orgs = Organization::getListData('id', 'name', false);
 ?>
 <div class="row">
     <div class="table-responsive">
@@ -21,63 +33,46 @@ use common\helpers\Lang;
                     <th class="dt-category-name">Countries</th>
                     <td colspan="4"></td>
                 </tr>
+                <?php foreach ($countries as $k => $name): ?>
                 <tr>
-                    <th class="dt-row-name">Kenya</th>
-                    <td>300</td>
-                    <td>250</td>
-                    <td>200</td>
-                    <td>200</td>
+                    <th class="dt-row-name"><?= Html::encode($name) ?></th>
+                    <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['country_id' => $k])) ?></td>
+                    <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['farm_type' => 'LSF', 'country_id' => $k])) ?></td>
+                    <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['farm_type' => 'SSF', 'country_id' => $k])) ?></td>
+                    <td><?= Yii::$app->formatter->asDecimal(Animal::getDashboardStats(Animal::STATS_ALL_TIME, false, [],  'created_at', null, null, ['country_id' => $k])) ?></td>
                 </tr>
-                <tr>
-                    <th class="dt-row-name">Tanzania</th>
-                    <td>300</td>
-                    <td>250</td>
-                    <td>200</td>
-                    <td>200</td>
-                </tr>
-                <tr>
-                    <th class="dt-row-name">Ethiopia</th>
-                    <td>300</td>
-                    <td>250</td>
-                    <td>200</td>
-                    <td>200</td>
-                </tr>
+                <?php endforeach; ?>
+
                 <tr>
                     <th class="dt-category-name">Organizations</th>
                     <td colspan="4"></td>
                 </tr>
-                <tr>
-                    <th class="dt-row-name">KALRO</th>
-                    <td>300</td>
-                    <td>250</td>
-                    <td>200</td>
-                    <td>200</td>
-                </tr>
-                <tr>
-                    <th class="dt-row-name">KLBA</th>
-                    <td>300</td>
-                    <td>250</td>
-                    <td>200</td>
-                    <td>200</td>
-                </tr>
+
+                <?php foreach ($orgs as $k => $name): ?>
+                    <tr>
+                        <th class="dt-row-name"><?= Html::encode($name) ?></th>
+                        <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['org_id' => $k])) ?></td>
+                        <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['farm_type' => 'LSF', 'org_id' => $k])) ?></td>
+                        <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['farm_type' => 'SSF', 'org_id' => $k])) ?></td>
+                        <td><?= Yii::$app->formatter->asDecimal(Animal::getDashboardStats(Animal::STATS_ALL_TIME, false, [],  'created_at', null, null, ['org_id' => $k])) ?></td>
+                    </tr>
+                <?php endforeach; ?>
                 <tr>
                     <th class="dt-category-name">Projects</th>
                     <td colspan="4"></td>
                 </tr>
-                <tr>
-                    <th class="dt-row-name">AVCD</th>
-                    <td>300</td>
-                    <td>250</td>
-                    <td>200</td>
-                    <td>200</td>
-                </tr>
-                <tr>
-                    <th class="dt-row-name">ADGG</th>
-                    <td>300</td>
-                    <td>250</td>
-                    <td>200</td>
-                    <td>200</td>
-                </tr>
+                <?php foreach ($projects as $k => $name): ?>
+                    <tr>
+                        <th class="dt-row-name"><?= Html::encode($name) ?></th>
+                        <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['project' => $name])) ?></td>
+                        <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['farm_type' => 'LSF', 'project' => $name])) ?></td>
+                        <td><?= Yii::$app->formatter->asDecimal(Farm::getDashboardStats(Farm::STATS_ALL_TIME, false, [],  'created_at', null, null, ['farm_type' => 'SSF', 'project' => $name])) ?></td>
+                        <?php
+                            $count = Animal::find()->select('id')->joinWith('farm')->andWhere(Farm::tableName(). '.project = :project', [':project' => $name])->count();
+                        ?>
+                        <td><?= Yii::$app->formatter->asDecimal($count) ?></td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
