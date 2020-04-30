3eacbee732e7f17810ccfe73cd62b948530282e6
Fred <mconyango@gmail.com>
2020-04-30 06:58:18 +0300

    Implementation for various Mistro Dbs migration (code improvements)

--- src/console/dataMigration/mistro/stanley1/Migrate.php
+++ src/console/dataMigration/mistro/stanley1/Migrate.php
@@ -19,11 +19,11 @@ class Migrate implements MigrateInterface
     public static function run()
     {
         //Clients::migrateData();
-        /*Farms::migrateData();
+        Farms::migrateData();
         Herds::migrateData();
         Cows::migrateData();
         Bulls::migrateData();
-        Lacts::migrateData();*/
+        Lacts::migrateData();
         Cowtests::migrateData();
         Cows::updateSiresAndDams();
     }
