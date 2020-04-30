beaa2069932ca3894b9615ed62dffd580a912113
Fred <mconyango@gmail.com>
2020-04-30 15:29:18 +0300

    Bug fix on Stanley2 db migration.

--- src/console/dataMigration/mistro/stanley2/Herds.php
+++ src/console/dataMigration/mistro/stanley2/Herds.php
@@ -2,6 +2,9 @@
 
 namespace console\dataMigration\mistro\stanley2;
 
+use backend\modules\core\models\Farm;
+use console\dataMigration\mistro\Helper;
+
 class Herds extends \console\dataMigration\mistro\klba\Herds
 {
     use MigrationTrait;
@@ -9,5 +12,22 @@ class Herds extends \console\dataMigration\mistro\klba\Herds
     public static function getFarmMigrationIdPrefix()
     {
         return Farms::getMigrationIdPrefix();
+    }
+
+    /**
+     * @param string $oldFarmId
+     * @return string|null
+     * @throws \Exception
+     */
+    public static function getFarmId($oldFarmId)
+    {
+        $oldFarmId = '24180001';
+        $migrationId = Helper::getMigrationId($oldFarmId, \console\dataMigration\mistro\stanley1\Farms::getMigrationIdPrefix());
+
+        $farmId = Farm::getScalar('id', ['migration_id' => $migrationId]);
+        if (empty($farmId)) {
+            return null;
+        }
+        return $farmId;
     }
 }
