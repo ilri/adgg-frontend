75a990dcbf0de8c7fc10c8c2981352b8d03a5c04
Fred <mconyango@gmail.com>
2020-05-02 15:59:14 +0300

    Calving Date validation (bug fix).

--- src/backend/modules/core/models/AnimalEventValidators.php
+++ src/backend/modules/core/models/AnimalEventValidators.php
@@ -29,13 +29,21 @@ trait AnimalEventValidators
             $dateDiff = DateUtils::getDateDiff($eventDate, $lastCalving->event_date);
             $minDays = 220;
             $maxDays = 1500;
-            if ($dateDiff->days < $minDays) {
-                $this->addError($attribute, Lang::t("Calving date interval should not be less than {minDays} days.", [
+            //last calving days should not be less than 220 days
+            //EXAMPLE SELECT * FROM  core_animal_event where animal_id="2483" AND event_type=1 AND DATEDIFF('2018-11-30',event_date)<220 AND id<>1428112;
+            $condition = "[[animal_id]]=:animal_id AND [[event_type]]=:event_type AND DATEDIFF(:event_date,[[event_date]])>0 AND DATEDIFF(:event_date,[[event_date]])<:min_days";
+            $params = [':animal_id' => $this->animal_id, ':event_type' => $this->event_type, ':event_date' => $this->event_date, ':min_days' => $minDays];
+            if (static::exists($condition, $params)) {
+                $this->addError($attribute, Lang::t("Last calving date interval should not be less than {minDays} days.", [
                     'minDays' => $minDays,
                 ]));
             }
-            if ($dateDiff->days > $maxDays) {
-                $this->addError($attribute, Lang::t("Calving date interval should not be more than {maxDays} days.", [
+            //next calving days should not be more than 1500 days
+            //EXAMPLE: SELECT * FROM core_animal_event where animal_id="2483" AND event_type=1 AND DATEDIFF(event_date,'2017-06-06')>0 AND DATEDIFF(event_date,'2017-06-06')>1500;
+            $condition = "[[animal_id]]=:animal_id AND [[event_type]]=:event_type AND DATEDIFF([[event_date]],:event_date)>:max_days";
+            $params = [':animal_id' => $this->animal_id, ':event_type' => $this->event_type, ':event_date' => $this->event_date, ':max_days' => $maxDays];
+            if (static::exists($condition, $params)) {
+                $this->addError($attribute, Lang::t("Next calving date interval should not be more than {maxDays} days.", [
                     'maxDays' => $maxDays,
                 ]));
             }
