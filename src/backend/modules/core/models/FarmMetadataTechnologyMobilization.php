9f39f1cc80bc5e7f5d25a1f445da136735514595
Fred <mconyango@gmail.com>
2020-07-09 17:28:54 +0300

    Farm metadata: Technology Mobilization

---
+++ src/backend/modules/core/models/FarmMetadataTechnologyMobilization.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: Fred <mconyango@gmail.com>
+ * Date: 2020-07-09
+ * Time: 5:25 PM
+ */
+
+namespace backend\modules\core\models;
+
+
+class FarmMetadataTechnologyMobilization extends FarmMetadata
+{
+
+    public static function getDefineMetadataType(): int
+    {
+        return self::TYPE_TECHNOLOGY_MOBILIZATION;
+    }
+}
