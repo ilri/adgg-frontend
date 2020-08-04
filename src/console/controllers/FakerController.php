7de9ea1bee4d01e005493025a0657fc00301e423
Fred <mconyango@gmail.com>
2020-08-04 17:41:06 +0300

    Support task: write a script to update farms without locations to location of animals

--- src/console/controllers/FakerController.php
+++ src/console/controllers/FakerController.php
@@ -20,9 +20,51 @@ class FakerController extends Controller
     public function actionTest()
     {
         //\console\jobs\ODKFormProcessor::push(['itemId' => 7794]);
-        $this->resetModels(Animal::class);
+        //$this->resetModels(Animal::class);
+        $this->setFarmLocationDetails();
         //\console\jobs\ODKFormProcessor::push(['itemId' => 8494]);
-       // $this->resetAnimals();
+        // $this->resetAnimals();
+    }
+
+    protected function setFarmLocationDetails()
+    {
+        $condition = '[[region_id]] IS NOT NULL';
+        $params = [];
+        $query = Animal::find()->andWhere($condition, $params);
+        $totalRecords = $query->count();
+        $n = 1;
+        $modelClassName = Animal::class;
+        /* @var $models Animal[] */
+        foreach ($query->batch(1000) as $i => $models) {
+            foreach ($models as $model) {
+                if ($model->farm !== null) {
+                    if (empty($model->farm->country_id)) {
+                        $model->farm->country_id = $model->country_id;
+                    }
+                    if (empty($model->farm->region_id)) {
+                        $model->farm->region_id = $model->region_id;
+                    }
+                    if (empty($model->farm->district_id)) {
+                        $model->farm->district_id = $model->district_id;
+                    }
+                    if (empty($model->farm->ward_id)) {
+                        $model->farm->ward_id = $model->ward_id;
+                    }
+                    if (empty($model->farm->village_id)) {
+                        $model->farm->village_id = $model->village_id;
+                    }
+                    if (empty($model->farm->org_id)) {
+                        $model->farm->org_id = $model->org_id;
+                    }
+                    if (empty($model->farm->client_id)) {
+                        $model->farm->client_id = $model->client_id;
+                    }
+                    $model->farm->save(false);
+                }
+                $this->stdout("{$modelClassName}: processed record {$n} of {$totalRecords} records\n");
+                $n++;
+            }
+        }
     }
 
     /**
