1439f8211deb8f6340cfc83c48dde0883ddfd2bd
Eric <eric@btimillman.com>
2020-09-24 17:32:12 +0300

    remove bright colors from other charts

--- src/backend/modules/dashboard/assets/src/js/module.js
+++ src/backend/modules/dashboard/assets/src/js/module.js
@@ -176,10 +176,9 @@ MyApp.modules.dashboard = {};
                 '#771957', '#7986CB', '#7F5298', '#65B27C',
                 '#056030', '#2B7B48', '#27921E', '#81D097',
                 '#6298D7', '#45ADC3', '#2EAB86', '#489661',
-                '#177380', '#D3E36F', '#DBB450', '#C97434',
-                '#AE2921', '#8C2B16', '#F00C0C', '#350d36',
-                '#EB6060', '#E39494', '#9C0204', '#853536',
-                '#C25D55', '#FF9900', '#875F03', '#F6FF00',
+                '#177380', '#C97434', '#AE2921', '#8C2B16',
+                '#853536', '#EB6060', '#E39494',
+                '#C25D55', '#875F03', '#350d36',
                 '#800080', '#902C8E', '#A0479D', '#AF60AC',
                 '#BE78BB', '#CD90C9', '#DCA8D9', '#EBC0E8',
                 '#FAD8F7', '#000000', '#1E1E1E', '#363636',
@@ -187,54 +186,6 @@ MyApp.modules.dashboard = {};
                 '#C3C3C3', '#E2E2E2', '#ECBEB3', '#FFD7CD',
                 '#641E16', '#783429', '#8B4A3E', '#9F6054',
                 '#B2776A', '#C58E82', '#D9A69A', '#C6E6FF',
-            ],
-            color: [
-                // countries
-                '#875F03', '#65B27C', '#5DB4A5',
-                // Holsteins
-                '#7F5298',
-                '#800080', '#902C8E', '#A0479D', '#AF60AC',
-
-                //Holstein Crosses
-                '#EBC0E8',
-                '#BE78BB', '#CD90C9', '#DCA8D9', '#EBC0E8',
-                //jerseys
-                '#C25D55',
-                '#B2776A', '#C58E82', '#D9A69A',
-                '#641E16', '#783429', '#8B4A3E', '#9F6054',
-                //Guernsey
-                '#A07D62',
-                //Ayrshires
-                '#C97434',
-                '#E3955A', '#FFBF8E', '#A5561A', '#7D3701',
-                '#c97a34', '#A55B1A',
-                //Ayrshire crosses
-                '#DBB450',
-                //Brown Swiss
-                '#771957',
-                //jersey Guernsey crosses
-                '#D2C966',
-                //Ind Zebu
-                '#245A62',
-                //Ind Zebu Tzn
-                '#509D99',
-                '#0d81e2','#0e87eb','#138cf1','#1c90f2',
-                '#2695f2','#309af3','#399ef3','#43a3f4','#4ca8f4',
-                '#56adf5',
-                //Ind Zebu Ken
-                '#9BAAD8',
-                //Ind Zebu Eth
-                '#0A60A8',
-                //Ind Sanga
-                '#65B27C',
-                //Exotic B Taurus Dual purpose
-                '#2F158B',
-                '#336699', '#1a334d', '#8cb3d9', '',
-                //Unknown - use a lighter gray
-                '#363636',
-                '#4F4F4F', '#6A6A6A', '#878787',
-                '#7D3701', '#641E16', '#27921E',
-                '#FFBF8E', '#489661', '#C97434',
             ]
         };
         this.container = container;
