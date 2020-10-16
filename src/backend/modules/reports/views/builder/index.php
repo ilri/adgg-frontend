b876bd206b96d9e259c7cd09a52e34560ea4cc78
Fred <mconyango@gmail.com>
2020-10-16 15:37:01 +0300

    Odk form processor: Bug fixes

--- src/backend/modules/reports/views/builder/index.php
+++ src/backend/modules/reports/views/builder/index.php
@@ -95,6 +95,7 @@ $this->params['breadcrumbs'][] = $this->title;
                                                 if (count($modelData['relations'])) {
                                                     $relations = $modelData['relations'];
                                                     $sub_relations = $modelData['sub_relations'] ?? [];
+                                                    dd($relations);
                                                     foreach ($relations as $relationName) {
                                                         $relation = $class->getRelation($relationName);
                                                         /* @var $relationModelClass ActiveRecord */
