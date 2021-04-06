338f4bb2199380a97ee8f9f222985bd89d8af90e
Fred <mconyango@gmail.com>
2021-04-06 15:00:50 +0300

    Bug fix: Farm metadata: Animal events

--- src/backend/modules/core/assets/src/js/module.js
+++ src/backend/modules/core/assets/src/js/module.js
@@ -55,35 +55,39 @@ MyApp.modules.core = {};
     }
 
     let showPretty = function (el) {
-        let data = $(el).data('json');
-        if (MyApp.utils.empty(data)) {
-            return;
-        }
+        try {
+            let data = $(el).data('json');
+            if (MyApp.utils.empty(data)) {
+                return;
+            }
 
-        function syntaxHighlight(json) {
-            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-            return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-                let cls = 'number';
-                if (/^"/.test(match)) {
-                    if (/:$/.test(match)) {
-                        cls = 'key';
-                    } else {
-                        cls = 'string';
+            function syntaxHighlight(json) {
+                json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+                    let cls = 'number';
+                    if (/^"/.test(match)) {
+                        if (/:$/.test(match)) {
+                            cls = 'key';
+                        } else {
+                            cls = 'string';
+                        }
+                    } else if (/true|false/.test(match)) {
+                        cls = 'boolean';
+                    } else if (/null/.test(match)) {
+                        cls = 'null';
                     }
-                } else if (/true|false/.test(match)) {
-                    cls = 'boolean';
-                } else if (/null/.test(match)) {
-                    cls = 'null';
-                }
-                return '<span class="' + cls + '">' + match + '</span>';
-            });
-        }
+                    return '<span class="' + cls + '">' + match + '</span>';
+                });
+            }
 
-        if (typeof data === "string") {
-            data = JSON.parse(data);
+            if (typeof data === "string") {
+                data = JSON.parse(data);
+            }
+            let str = JSON.stringify(data, undefined, 4);
+            $(el).html(syntaxHighlight(str));
+        } catch (e) {
+            console.log(e);
         }
-        let str = JSON.stringify(data, undefined, 4);
-        $(el).html(syntaxHighlight(str));
     }
 
     ODK_DATA_VIEW.prototype.showPretty = function () {
