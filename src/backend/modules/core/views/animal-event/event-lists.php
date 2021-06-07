a800cf0309bff578cc3b40a0d54d4e688942a11f
Mirieri Mogaka <mirierimogaka@ILRIKE-22721-MacBook-Pro.local>
2021-06-07 15:24:51 +0300

    Changes to include feeding

--- src/backend/modules/core/views/animal-event/event-lists.php
+++ src/backend/modules/core/views/animal-event/event-lists.php
@@ -51,6 +51,8 @@ $this->params['breadcrumbs'] [] = $this->title;
                         $url = 'synchronization-event/index';
                     } elseif ($key == AnimalEvent::EVENT_TYPE_WEIGHTS) {
                         $url = 'weight-event/index';
+                    }elseif ($key == AnimalEvent::EVENT_TYPE_FEEDING) {
+                        $url = 'feeding-event/index';
                     } elseif ($key == AnimalEvent::EVENT_TYPE_HEALTH) {
                         $url = 'health-event/index';
                     } elseif ($key == AnimalEvent::EVENT_TYPE_EXITS) {
