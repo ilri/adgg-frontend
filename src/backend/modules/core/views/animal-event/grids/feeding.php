101b7a361748ca4feb548b20371f8fca706ba6d4
Mirieri Mogaka <mirierimogaka@ILRIKE-22721-MacBook-Pro.local>
2021-06-11 23:29:46 +0300

    fixing bug on feed_given for the reports that needs to be an array

--- src/backend/modules/core/views/animal-event/grids/feeding.php
+++ src/backend/modules/core/views/animal-event/grids/feeding.php
@@ -48,7 +48,7 @@ use yii\web\View;
             'attribute' => 'feed_given',
             'label'=>'feed_given',
             'value' => function (AnimalEvent $model) {
-                 return array(Choices::getLabel(ChoiceTypes::CHOICE_TYPE_FEED_TYPE, $model->feed_given));
+                 return Choices::getMultiSelectLabel($model->feed_given,ChoiceTypes::CHOICE_TYPE_FEED_TYPE);
             },
             'enableSorting' => false,
         ],
