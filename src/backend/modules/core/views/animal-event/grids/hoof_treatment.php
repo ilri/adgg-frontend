ccd1247374f7d9a4c1d9d98bc79b66e07243e461
dmogaka <davemirieri@gmail.com>
2021-11-28 06:40:20 +0300

    Changes to include field agent id on other events

--- src/backend/modules/core/views/animal-event/grids/hoof_treatment.php
+++ src/backend/modules/core/views/animal-event/grids/hoof_treatment.php
@@ -18,6 +18,12 @@ use yii\web\View;
     ],
     'columns' => [
         [
+            'attribute' => 'field_agent_id',
+            'value' => function (AnimalEvent $model) {
+                return $model->getRelationAttributeValue('fieldAgent', 'name');
+            }
+        ],
+        [
             'attribute' => 'event_date',
             'label' => 'Hoof Treatment Event Date',
             'format' => ['date', 'php:d-M-Y'],
