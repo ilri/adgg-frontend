8f9f641fc5543569cdbd57631f88c21ea22b55ba
Mirieri Mogaka <mirierimogaka@Mirieris-MacBook-Pro.local>
2022-03-21 21:24:54 +0300

    Changes to include correct the navigation bar test five

--- src/backend/modules/core/views/farm/index.php
+++ src/backend/modules/core/views/farm/index.php
@@ -2,26 +2,19 @@
 
 use backend\modules\core\models\Country;
 use backend\modules\core\models\Farm;
-use backend\modules\core\models\CountryUnits;
 
 /* @var $this \yii\web\View */
 /* @var $searchModel Farm */
 /* @var $country \backend\modules\core\models\Country */
 /* @var $controller \backend\controllers\BackendController */
-///* @var $district \backend\modules\core\models\CountryUnits*/
 
 
 $controller = Yii::$app->controller;
 
 $this->title = $controller->getPageTitle();
-
 if ($country) {
     $this->params['breadcrumbs'][] = Country::getScalar('name', ['id' => $country]);
 }
-//else if ($district){
-//    $this->params['breadcrumbs'][] = CountryUnits::getScalar('name', ['id' => Session::getDistrictId(), 'level' => CountryUnits::LEVEL_DISTRICT], ['id' => $district]);
-//}
-
 $this->params['breadcrumbs'] [] = $this->title;
 ?>
 <div class="row">
