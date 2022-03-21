cb44fb58df1dd4d70f5bbf4c6136efac38603772
Mirieri Mogaka <mirierimogaka@Mirieris-MacBook-Pro.local>
2022-03-21 19:50:20 +0300

    Changes to include correct the navigation bar two

--- src/backend/modules/core/views/farm/index.php
+++ src/backend/modules/core/views/farm/index.php
@@ -18,15 +18,15 @@ $this->title = $controller->getPageTitle();
 if ($country) {
     $this->params['breadcrumbs'][] = Country::getScalar('name', ['id' => $country]);
 }
-else if ($district){
-    $this->params['breadcrumbs'][] = CountryUnits::getScalar('name', ['id' => Session::getDistrictId(), 'level' => CountryUnits::LEVEL_DISTRICT]);
-}
+//else if ($district){
+//    $this->params['breadcrumbs'][] = CountryUnits::getScalar('name', ['id' => Session::getDistrictId(), 'level' => CountryUnits::LEVEL_DISTRICT]);
+//}
 $this->params['breadcrumbs'] [] = $this->title;
 ?>
 <div class="row">
     <div class="col-lg-12">
         <?= $this->render('@coreModule/views/farm/_tab', ['model' => $searchModel, 'country' => !empty($country) ? $country : null]) ?>
-
+        <?= $this->render('@coreModule/views/farm/_tab', ['model' => $searchModel, CountryUnits::getScalar('name', ['id' => Session::getDistrictId(), 'level' => CountryUnits::LEVEL_DISTRICT])]) ?>
         <div class="tab-content">
             <?= $this->render('_filter', ['model' => $searchModel,]) ?>
             <?= $this->render('_grid', ['model' => $searchModel]) ?>
