b71968a6ededda07ff595e60de8f8cbe4e371bb6
Mirieri David Mogaka <davemirieri@gmail.com>
2022-11-14 14:56:53 +0300

    changes to solve calving error

--- src/console/jobs/ODKFormProcessor.php
+++ src/console/jobs/ODKFormProcessor.php
@@ -576,7 +576,7 @@ class ODKFormProcessor extends BaseObject implements JobInterface
             }
             $newAnimalModel->setDynamicAttributesValuesFromOdkForm($animalData, $animalIdentificationGroupKey, $repeatKey);
             $damModel = $this->getOrRegisterAnimalDam($animalData, $farmModel, $k);
-            Yii::info(json_encode($damModel), "changes on whether dam record is being captured");
+            Yii::info(json_encode($animalData), "changes on whether dam record is being captured");
             if (null !== $damModel) {
                 $newAnimalModel->dam_id = $damModel->id;
                 $newAnimalModel->dam_tag_id = $damModel->tag_id;
