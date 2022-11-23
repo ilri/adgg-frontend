8842d5897f4466e4e1a140b08de70e9659afcd24
Mirieri David Mogaka <davemirieri@gmail.com>
2022-11-23 08:41:47 +0300

    Changes to implement tagid validation

--- src/console/jobs/ODKFormProcessor.php
+++ src/console/jobs/ODKFormProcessor.php
@@ -1825,11 +1825,7 @@ class ODKFormProcessor extends BaseObject implements JobInterface
      */
     protected function saveAnimalModel($model, $index, $validate = true)
     {
-        $newModel = Animal::find()
-            ->andwhere(['tag_id' => $model->tag_id])
-            ->andwhere(['name' => $model->name])
-            ->andwhere(['village_id' => $model->village_id])
-            ->one();
+        $newModel = Animal::find()->andWhere(['tag_id' => $model->tag_id])->one();
         Yii::info(json_encode($newModel), "testing to check how the where condition is executed");
         if ($newModel !== null) {
             $newModel->ignoreAdditionalAttributes = false;
@@ -1841,7 +1837,6 @@ class ODKFormProcessor extends BaseObject implements JobInterface
         } else {
             $newModel = clone $model;
         }
-//        $newModel = clone $model;
         $data = $this->saveModel($newModel, $validate);
         $this->_animalsData[$index] = $data['data'];
         $this->_animalsModels[$index] = $data['model'];
